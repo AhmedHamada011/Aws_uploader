ea10802cc380f53760b1a4ad20d1bcba63fff78c
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-03-14 00:35:37 +0200

    refactor: use constants instead of variables

--- classes/AwsUploader.php
+++ classes/AwsUploader.php
@@ -4,26 +4,17 @@ use Aws\S3\S3Client;
 
 class AwsUploader{
 
-  private $key;
-  private $secret;
-  private $region;
-  private $bucket_name;
   private $s3;
 
   public function __construct()
   {
-    $this->key = AWS_KEY;
-    $this->secret = AWS_KEY_SECRET;
-    $this->region = AWS_REGION;
-    $this->bucket_name = AWS_BUCKET_NAME;
-
     $this->initialize_s3_client();
   }
 
   public function upload($file_name,$source_file){
     
     $result = $this->s3->putObject([
-			'Bucket' => $this->bucket_name,
+			'Bucket' => AWS_BUCKET_NAME,
 			'Key'    => $file_name,
 			'SourceFile' => $source_file			
 		]);
@@ -35,9 +26,9 @@ class AwsUploader{
   private function initialize_s3_client(){
     $this->s3 = S3Client::factory(array(
       'credentials' => array(
-          'key' => $this->key,
-          'secret' => $this->secret,
-      ), 'region' => $this->region,
+          'key' => AWS_KEY,
+          'secret' => AWS_KEY_SECRET,
+      ), 'region' => AWS_REGION,
       'version' => 'latest'
   ));
 
